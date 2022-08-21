ce2e853da14a76b6a326c8cda42fd29259997831
nickgiggey <nickgiggey@comcast.net>
2022-08-20 18:19:10 -0600

    code cleanup

--- password-app/src/App.js
+++ password-app/src/App.js
@@ -3,21 +3,19 @@ import './css/reset.css'
 import "./css/App.css";
 import './css/hostAlgorithm.css'
 import IpAddress from './ipData/ipAddress';
-import FrequencyHop from './hostAlgorithmComponent/frequencyHop';
 import FingerPrint from './fingerPrint/fingerPrint';
 import BioID from './bioID/bioID';
 import IpGeo from './ipData/ipGeo';
 
-export const App = () => {
+function App() {
   const [style, setStyle] = useState("cont");
   const changeStyle = () => {
-    console.log("Connected");
     setStyle("cont2");
   };
   return (
-    <section>
+    <section className="App-main-container">
       <div className="App"></div>
-      <header className="einstein">instein </header>
+      <header className="einstein">instein</header>
       <nav className={style}>
         <i className="material-icons md-36 md-light arrow"></i>
         <ul className="menu-box-wrapper">
@@ -27,11 +25,10 @@ export const App = () => {
           <li className="ipgeo-main-container">
             <IpGeo />
           </li>
-          <li className="frequencyhop-main-container">
-            <FrequencyHop />
-          </li>
         </ul>
-        <button className="buttonTwo" onClick={changeStyle}><FingerPrint /></button>
+        <button className="buttonTwo" onClick={changeStyle}>
+          <FingerPrint />
+        </button>
         <BioID />
       </nav>
     </section>
--- password-app/src/fingerPrint/fingerPrint.js
+++ password-app/src/fingerPrint/fingerPrint.js
@@ -2,23 +2,26 @@ import React, {useState} from 'react';
 import '../css/fingerPrint.css';
 function FingerPrint() {
     const [num, setNum] = useState(0);
+    const [num2, setNum2] = useState(0);
 
-    function randomNumberInRange(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+    function randoNumber(min, max) {
+        return Math.floor(Math.random() * ((max - min + 1)) * Math.PI) + min;
     }
 
     const handleClick = () => {
-        setNum(randomNumberInRange(1, 100));
+        setNum(randoNumber(1, 100));
+        setNum2(randoNumber(1, 100))
     };
+
     return (
-        <div>
+        <div className="fingerPrint-main-container">
             <h2>number is: {num}</h2>
+            <h2>number2 is: {num2}</h2>
             <button onClick={handleClick}
                 className="btn"
                 type='Submit'
                 required
             >
-                {' '}
                 <i className="fas fa-fingerprint i1" aria-hidden="true"></i>
             </button>
         </div>
